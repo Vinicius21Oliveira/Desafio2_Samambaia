e39fc199593213c518987309abf21606902089c7
Vinicius Oliveira <viniciusoliveiraa8004@gmail.com>
2024-08-21 00:00:43 -0300

    Criando a classe FlorSamambaia

---
+++ src/principal/FlorSamambaia.java
@@ -0,0 +1,15 @@
+package principal;
+
+public class FlorSamambaia {
+	
+	public static String tentativaDesenhar(int r1, int x1, int y1, int r2, int x2, int y2) {
+        
+		double distanciaCentros = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if (distanciaCentros + r2 <= r1) {
+            return "RICO";
+        } else {
+            return "MORTO";
+        }
+    }
+}
