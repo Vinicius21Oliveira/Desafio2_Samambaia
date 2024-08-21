2205480356ad019fbbee1806235c58771fed4774
Vinicius Oliveira <viniciusoliveiraa8004@gmail.com>
2024-08-21 00:05:00 -0300

    Criando a classe principal

---
+++ src/principal/Main.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+     System.out.println(FlorSamambaia.tentativaDesenhar(5, 0, 0, 2, 1, 1));
+	 System.out.println(FlorSamambaia.tentativaDesenhar(3, 0, 0, 2, 4, 4)); 
+    }
+}
